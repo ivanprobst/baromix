bd9cd3dcb9d07abcbb6119494e97a92b96a59038
Ivan Probst <ivan.probst@gmail.com>
2022-02-08 19:51:07 +0100

    set up Jest and testing library

---
+++ baromix-front/tests/HomeForm.test.ts
@@ -0,0 +1,9 @@
+// import { render, fireEvent } from "@testing-library/vue";
+
+// import HomeForm from "../src/components/HomeForm.vue";
+
+describe("testing home...", () => {
+  it("just works", async () => {
+    console.log("testing...");
+  });
+});

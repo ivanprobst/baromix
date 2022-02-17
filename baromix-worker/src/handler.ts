76bb5a14aa1b041b4eeae8fa1b88a64dbe08afbc
Ivan Probst <ivan.probst@gmail.com>
2022-02-17 18:06:25 +0100

    created basic worker routing for weather data processing

--- baromix-worker/src/handler.ts
+++ baromix-worker/src/handler.ts
@@ -1,5 +1,27 @@
+const sendDefaultResponse = async (request: Request): Promise<Response> => {
+  return new Response(`Successful call to url ${request.url}`);
+};
+
+const saveWeatherData = async (request: Request): Promise<Response> => {
+  const requestData = await request.json();
+
+  // TODO: save the data somewhere
+
+  const responseData = { success: true, weatherDataSaved: requestData };
+  return new Response(JSON.stringify(responseData));
+};
+
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(
-    `request method: ${request.method}, request url: ${request.url}`,
-  );
+  console.log("request received: ", {
+    url: request.url,
+    body: request.body,
+  });
+
+  const uri = request.url.replace(/^https:\/\/.*?\//gi, "/");
+
+  if (uri === "/weatherdata" && request.method === "POST" && request.body) {
+    return saveWeatherData(request);
+  }
+
+  return sendDefaultResponse(request);
 }
--- baromix-worker/test/handler.test.ts
+++ baromix-worker/test/handler.test.ts
@@ -1,18 +1,44 @@
-import { handleRequest } from '../src/handler'
-import makeServiceWorkerEnv from 'service-worker-mock'
+import { handleRequest } from "../src/handler";
+import makeServiceWorkerEnv from "service-worker-mock";
 
-declare var global: any
+declare let global: any;
 
-describe('handle', () => {
+describe("Testing workers handlers...", () => {
   beforeEach(() => {
-    Object.assign(global, makeServiceWorkerEnv())
-    jest.resetModules()
-  })
+    Object.assign(global, makeServiceWorkerEnv());
+    jest.resetModules();
+  });
 
-  test('handle GET', async () => {
-    const result = await handleRequest(new Request('/', { method: 'GET' }))
-    expect(result.status).toEqual(200)
-    const text = await result.text()
-    expect(text).toEqual('request method: GET')
-  })
-})
+  test("the default routing...", async () => {
+    const result = await handleRequest(new Request("/", { method: "GET" }));
+
+    const text = await result.text();
+
+    expect(result.status).toEqual(200);
+    expect(text).toEqual("Successful call to url https://www.test.com/");
+  });
+
+  test("the form data processing routing...", async () => {
+    const MOCK_WEATHER_DATA = {
+      inputBarometer: "12",
+      inputDate: "23",
+      inputTime: "24",
+      selectedWeatherTags: ["cloudy"],
+    };
+
+    const result = await handleRequest(
+      new Request("/weatherdata", {
+        method: "POST",
+        body: JSON.stringify(MOCK_WEATHER_DATA),
+      }),
+    );
+
+    const status = await result.json();
+
+    expect(result.status).toEqual(200);
+    expect(status).toEqual({
+      success: true,
+      weatherDataSaved: MOCK_WEATHER_DATA,
+    });
+  });
+});
